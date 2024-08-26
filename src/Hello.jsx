32f3e4e413928a3d04b1a148c07af705a546c997
KARRI VENKATESH  1000 <12914096-KARRI-VENKATESH@users.noreply.replit.com>
2024-08-26 16:38:01 +0000

    react project
    react project

---
+++ src/Hello.jsx
@@ -0,0 +1,12 @@
+// src/components/HelloWorld.jsx
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
